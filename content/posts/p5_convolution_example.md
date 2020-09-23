0ada1a0561dbf8b9182d4aacbaea1fb1cb39d61a
juanda <jdbarraganm@unal.edu.com>
2020-09-23 08:38:55 -0500

    convolution2

--- content/posts/p5_convolution_example.md
+++ content/posts/p5_convolution_example.md
@@ -5,8 +5,33 @@ images: []
 draft: false
 tags: ["P5", "JS" , "Hugo"]
 ---
+<div>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ex non ipsum rutrum faucibus in ut orci. Vestibulum vel orci orci. Curabitur vitae risus tincidunt, tristique neque at, dignissim arcu. Morbi euismod mollis lacus sed sagittis. Morbi lacinia pharetra felis, sit amet porttitor nunc iaculis non. Donec bibendum maximus dui eu facilisis. Donec ultrices placerat urna a convallis. Nunc et ipsum luctus, consequat sem a, pulvinar felis. Integer posuere tincidunt urna, at fermentum nulla accumsan quis. Sed dignissim, purus eget efficitur vestibulum, sapien leo sodales mauris, id auctor neque turpis quis lacus. Aenean et nunc purus. Ut hendrerit mi tortor, vel dapibus mi feugiat a. Quisque ultricies, eros id faucibus pulvinar, justo enim lobortis diam, pulvinar auctor nunc quam non dolor. Sed scelerisque nulla leo, et tincidunt nunc blandit sit amet.
+
 
 <link rel="stylesheet" href="style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js" type="text/javascript"></script>
-<script src="/js/convolution.js" type="text/javascript"></script>
+
+<div class="row">
+    <div style="float: left; width: 50%;">
+        <img src="/images/rover.png" alt="Italian Trulli">
+    </div>
+    <div style="float: left; width: 50%;" id="myCanvas">
+        <script src="/js/convolution2.js" type="text/javascript">
+        </script>     
+    </div>
+</div>
+<div class="row">
+    <select name="efeects" id="efects">
+        <option value="blur">blur</option>
+        <option value="bottom sobel">bottom sobel</option>
+        <option value="emboss">emboss</option>
+        <option value="identity">identity</option><option value="emboss">emboss</option>
+        <option value="left sobel">left sobel</option>
+        <option value="outline">outline</option>
+        <option value="right sobel">right sobel</option>
+        <option value="sharpen">sharpen</option>
+        <option value="top sobel">top sobel</option>
+    </select>
+</div>
+</div>
---
+++ static/js/convolution2.js
@@ -0,0 +1,121 @@
+var canvasDiv = document.getElementById('myCanvas');
+var width = canvasDiv.clientWidth;
+let ik = 0;
+let v = 1.0 / 9.0;
+let kernel = [[ v, v, v ], [ v, v, v ], [ v, v, v ]]; 
+let blurr = [[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]];
+let b_s = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]];
+let emboss = [[-2, -1, 0], [-1, 1, 1], [0, 1, 2]];
+let identity = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+let l_s = [[1, 0, -1], [2, 0, -2], [1, 0, -1]];
+let outline = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]];
+let r_s = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+let sharpen = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]];
+let t_s = [[1, 2, 1], [0, 0, 0], [-1, -2 -1]];
+
+
+
+let kernel2=[blurr,b_s,emboss,identity,l_s,outline,r_s,sharpen,t_s];
+
+
+function randomizeKernel() {
+  ik = (ik+1)%9;
+  console.log(ik);
+}
+/*
+$( document ).ready(function() {
+  var button2 = document.getElementById('boton');
+  button2.mousePressed(randomizeKernel());
+  button2.addEventListener("click", randomizeKernel());
+  
+});*/
+
+function preload() {
+  var loc = window.location.pathname;
+  var dir = loc.substring(0, loc.lastIndexOf('/'));
+  console.log(dir)
+  img = loadImage("/images/rover.png"); 
+  
+}
+
+function setup() {
+  var MyCanvas = createCanvas(img.width, img.height+100)
+  MyCanvas.parent("myCanvas");
+  background(60);
+  button = createButton('click me');
+  button.position(img.width, img.height);
+  button.mousePressed(changeBG);
+
+
+function changeBG() {
+  ik = (ik+1)%9;
+  console.log(ik);
+}
+  
+
+
+  noLoop();
+}
+
+
+function draw() {
+  //image(img, 0, 0);
+
+  
+  edgeImg = createImage(img.width, img.height);
+
+  edgeImg.loadPixels();
+  button = createButton('ver');
+  button.position(img.width, img.height);
+  
+  //button.mousePressed(convolution());
+  
+  for (let x = 1; x < img.width; x++) {
+    for (let y = 1; y < img.height; y++) {
+      let sum = 0; 
+
+      for (kx = -1; kx <= 1; kx++) {
+        for (ky = -1; ky <= 1; ky++) {
+          let xpos = x + kx;
+          let ypos = y + ky;
+          
+          let val = red(img.get(xpos, ypos));
+
+          sum += kernel2[ik][kx+1][ky+1] * val;
+        }
+      }
+
+      edgeImg.set(x, y, color(sum));
+    }
+  }
+  edgeImg.updatePixels();
+  
+  image(edgeImg, 0, 0);
+}
+/*
+function convolution() {
+  edgeImg = createImage(img.width, img.height);
+
+  edgeImg.loadPixels();
+  for (let x = 1; x < img.width; x++) {
+    for (let y = 1; y < img.height; y++) {
+      let sum = 0; 
+
+      for (kx = -1; kx <= 1; kx++) {
+        for (ky = -1; ky <= 1; ky++) {
+          let xpos = x + kx;
+          let ypos = y + ky;
+          
+          let val = red(img.get(xpos, ypos));
+
+          sum += kernel2[ik][kx+1][ky+1] * val;
+        }
+      }
+
+      edgeImg.set(x, y, color(sum));
+    }
+  }
+  edgeImg.updatePixels();
+  
+  image(edgeImg, 0, 0);
+} */
