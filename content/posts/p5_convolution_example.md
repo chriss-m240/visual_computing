57f8af17a3a4f07b1f343bf58f3188023cfd70ca
chris <camantillad@unal.edu.co>
2020-10-01 21:04:33 -0500

    refactor code

--- content/posts/p5_convolution_example.md
+++ content/posts/p5_convolution_example.md
@@ -9,19 +9,18 @@ tags: ["P5", "JS" , "Hugo"]
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ex non ipsum rutrum faucibus in ut orci. Vestibulum vel orci orci. Curabitur vitae risus tincidunt, tristique neque at, dignissim arcu. Morbi euismod mollis lacus sed sagittis. Morbi lacinia pharetra felis, sit amet porttitor nunc iaculis non. Donec bibendum maximus dui eu facilisis. Donec ultrices placerat urna a convallis. Nunc et ipsum luctus, consequat sem a, pulvinar felis. Integer posuere tincidunt urna, at fermentum nulla accumsan quis. Sed dignissim, purus eget efficitur vestibulum, sapien leo sodales mauris, id auctor neque turpis quis lacus. Aenean et nunc purus. Ut hendrerit mi tortor, vel dapibus mi feugiat a. Quisque ultricies, eros id faucibus pulvinar, justo enim lobortis diam, pulvinar auctor nunc quam non dolor. Sed scelerisque nulla leo, et tincidunt nunc blandit sit amet.
 
 
-<link rel="stylesheet" href="style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js" type="text/javascript"></script>
 
 <div class="row">
     <div class="centered_div">
         <img id="source_img" src="/images/rover.png" alt="Italian Trulli">
     </div>
-    <div class="centered_div" style="padding-top: 7px" id="myCanvas">
-        <script src="/js/convolution2.js" type="text/javascript"></script>     
+    <div class="centered_div" style="padding-top: 7px" id="canvas">
+        <script src="/js/convolution.js" type="text/javascript"></script>     
     </div>
 </div>
-    <div id="test">
-        <select name="cars" id="cars"> </select>
+    <div id="dropdown">
+        <select id="kernels"> </select>
     </div>
 </div>
 
@@ -36,7 +35,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ex non ipsum ru
     margin:auto
 }
 
-#test {
+#dropdown {
     text-align: center;
 }
 
--- static/js/convolution.js
+++ static/js/convolution.js
@@ -1,46 +1,115 @@
-let v = 1.0 / 9.0;
-let kernel = [[ v, v, v ], [ v, v, v ], [ v, v, v ]]; 
+let kernel = null;
+
+let blur_ = [[0.0625, 0.125, 0.0625],
+             [0.125, 0.25, 0.125],
+             [0.0625, 0.125, 0.0625]];
+
+let emboss = [[-2, -1, 0],
+              [-1, 1, 1],
+              [0, 1, 2]];
+
+let identity = [[0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0]];
+
+let sharpen = [[0, -1, 0], 
+               [-1, 5, -1],
+               [0, -1, 0]];
+
+let outline = [[-1, -1, -1],
+               [-1, 8, -1],
+               [-1, -1, -1]];
+
+let bottomSobel = [[-1, -2, -1],
+            [0, 0, 0],
+            [1, 2, 1]];
+
+let leftSobel = [[1, 0, -1],
+           [2, 0, -2],
+           [1, 0, -1]];
+
+let topSobel = [[1, 2, 1],
+           [0, 0, 0],
+           [-1, -2, -1]];  
+
+let rightSobel = [[-1, 0, 1], 
+           [-2, 0, 2],
+           [-1, 0, 1]];
+
+let kernels = {
+  "none" : null,
+  "blur" : blur_,
+  "emboss" : emboss,
+  "identity" : identity,
+  "sharpen" : sharpen,
+  "outline" : outline,
+  "bottom sobel" : bottomSobel,
+  "left sobel" : leftSobel,
+  "top sobel" : topSobel,
+  "right sobel" : rightSobel,
+}
+
+var selectedKernel = "none"
+var flag = false
 
 function preload() {
-  var loc = window.location.pathname;
-  var dir = loc.substring(0, loc.lastIndexOf('/'));
-  console.log(dir)
   img = loadImage("/images/rover.png"); 
 }
 
 function setup() {
-  createCanvas(710, 400).center('horizontal');
-  
-  noLoop();
+  var canvas = createCanvas(img.width, img.height)
+  canvas.parent("canvas");
+  background(60);
+
+  sel = createSelect(select("#kernels"));
+  sel.option('none');
+  sel.option('bottom sobel');
+  sel.option('emboss');
+  sel.option('identity');
+  sel.option('left sobel');
+  sel.option('outline');
+  sel.option('right sobel');
+  sel.option('sharpen');
+  sel.option('top sobel');
+
+  sel.selected("none");
+
+  sel.changed(handleSelectChange);
+
+  function handleSelectChange() {
+    selectedKernel = sel.value();
+    kernel = kernels[selectedKernel]
+    flag = true
+  }
 }
 
-
 function draw() {
-  image(img, 0, 0);
+  if(selectedKernel == undefined || selectedKernel == "none") {
+      image(img, 0, 0);
+  } else if (flag) {
+    edgeImg = createImage(img.width, img.height);
+    edgeImg.loadPixels();
+    
+    for (let x = 1; x < img.width; x++) {
+        for (let y = 1; y < img.height; y++) {
+        let sum = 0; 
   
-  edgeImg = createImage(img.width, img.height);
-
-  edgeImg.loadPixels();
-  
-  for (let x = 1; x < img.width; x++) {
-    for (let y = 1; y < img.height; y++) {
-      let sum = 0; 
-
-      for (kx = -1; kx <= 1; kx++) {
-        for (ky = -1; ky <= 1; ky++) {
-          let xpos = x + kx;
-          let ypos = y + ky;
-          
-          let val = red(img.get(xpos, ypos));
-
-          sum += kernel[kx+1][ky+1] * val;
+        for (kx = -1; kx <= 1; kx++) {
+            for (ky = -1; ky <= 1; ky++) {
+              let xpos = x + kx;
+              let ypos = y + ky;
+              
+              let val = red(img.get(xpos, ypos));
+    
+              sum += kernel[kx+1][ky+1] * val;
+            }
         }
-      }
-
-      edgeImg.set(x, y, color(sum));
-    }
-  }
-  edgeImg.updatePixels();
   
-  image(edgeImg, img.width, 0);
+        edgeImg.set(x, y, color(sum));
+        }
+    }
+    edgeImg.updatePixels();
+    image(edgeImg, 0, 0);
+    flag = false
+  }
 }
